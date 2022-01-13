db85a5ac81f188e83e3a47335e75a9007d5781de
Bojan Jagetic <jageticb@yahoo.com>
2022-01-13 03:08:07 +0100

    components made,

---
+++ components/PostWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostWidget = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PostWidget
---
+++ components/index.js
@@ -0,0 +1,3 @@
+export {default as PostCard} from './PostCard';
+export {default as PostWidget} from './PostWidget';
+export {default as Category} from './Categories';
