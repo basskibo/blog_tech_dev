4f51215e50fccd4cc000678866f858839bc2c60a
Bojan Jagetic <jageticb@yahoo.com>
2022-01-19 00:50:42 +0100

    comments list fetching and  rendering

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import parse from "html-react-parser";
 
-const Comment = () => {
-  return <div>Comments</div>;
+import { getComments } from "../services";
+const Comment = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, []);
+  return (
+    <div>
+      {" "}
+      {comments.length > 0 && (
+        <div className="bg-white dark:bg-slate-900  shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 dark:text-white font-semibold border-b pb-4">
+            {comments.length} Comments
+          </h3>
+          {comments.map((comment) => (
+            <div
+              key={comment.createdAt}
+              className="border-b border-slate-100 mb-4 pb-4"
+            >
+              <p className="mb-4 dark:text-slate-400">
+                <span className="text-white font-semibold">{comment.name}</span>{" "}
+                on {moment(comment.createdAt).format("MMMM DD, YYYY")}
+              </p>
+              <p className="dark: text-slate-400">{comment.comment} </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Comment;
--- services/index.js
+++ services/index.js
@@ -142,3 +142,18 @@ export const submitComment = async (obj) => {
   });
   return result.json();
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+  return result.comments;
+};
