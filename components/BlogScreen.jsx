b52a5ed21c185409277090b757cb068a85b9d655
Bojan <bojan.jagetic@rt-rk.com>
2022-01-28 10:53:48 +0100

    image support in mdx, px for blog, projects,post detail

--- components/BlogScreen.jsx
+++ components/BlogScreen.jsx
@@ -1,83 +1,77 @@
-import React, { useState, useEffect } from "react";
-import moment from "moment";
-import {
-  PostCard,
-  PostWidget,
-  Categories,
-  Pagination,
-  CategoryChip,
-} from "../components";
-import Accent from "./custom/Accent";
-const numberPerPage = 1;
+import React, { useState, useEffect } from "react"
+import moment from "moment"
+import { PostCard, PostWidget, Categories, Pagination, CategoryChip } from "../components"
+import Accent from "./custom/Accent"
+const numberPerPage = 1
 
 const BlogScreen = ({ posts }) => {
-  const [numberOfPages, setnumberOfPages] = useState(0);
-  const postNum = posts.length;
-  const [numberOfPosts, setNumberOfPosts] = useState(postNum);
-  const [pageNumber, setpageNumber] = useState(1);
+	const [numberOfPages, setnumberOfPages] = useState(0)
+	const postNum = posts.length
+	const [numberOfPosts, setNumberOfPosts] = useState(postNum)
+	const [pageNumber, setpageNumber] = useState(1)
 
-  useEffect(() => {
-    const offset = (pageNumber - 1) * numberPerPage + 1;
-    const calculatedPages = Math.ceil(posts.length / numberPerPage);
-    setnumberOfPages(calculatedPages);
-    setpageNumber(offset);
-  }, []);
+	useEffect(() => {
+		const offset = (pageNumber - 1) * numberPerPage + 1
+		const calculatedPages = Math.ceil(posts.length / numberPerPage)
+		setnumberOfPages(calculatedPages)
+		setpageNumber(offset)
+	}, [])
 
-  return (
-    <div className="container mx-auto my-14 px-5 sm:px-2 xs:px-3 lg:px-5 bg-gradient-to-tr text-slate-400">
-      {/* <div className='container mx-auto my-14 sm:px-0 xs:px-0 lg:px-5 bg-gradient-to-tr text-slate-400'> */}
-      <div className="my-10">
-        <h1 class="mb-5">
-          <Accent className="font-extrabold text-6xl">Blog</Accent>
-        </h1>
-        <p className="display-4">
-          Thoughts, mental models, and tutorials about all kind of web
-          development, devOps and hybrid mobile applications .
-        </p>
-      </div>
+	return (
+		<div className='container mx-auto my-14 px-5 sm:px-2 xs:px-3 lg:px-5 bg-gradient-to-tr text-slate-400'>
+			{/* <div className='container mx-auto my-14 sm:px-0 xs:px-0 lg:px-5 bg-gradient-to-tr text-slate-400'> */}
+			<div className='my-10'>
+				<h1 class='mb-5'>
+					<Accent className='font-extrabold text-6xl'>Blog</Accent>
+				</h1>
+				<p className='display-4'>
+					Thoughts, mental models, and tutorials about all kind of web development,
+					devOps and hybrid mobile applications .
+				</p>
+			</div>
 
-      <div class="grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 lg:gap-14 ">
-        {posts.map((post, index) => (
-          <div class="w-full   text-white rounded-md border border-gray-600 dark:bg-dark dark:border-gray-600 transform-gpu scale-100 hover:scale-[1.02] active:scale-[0.97] hover:cursor-pointer transition duration-100 animate-shadow">
-            <a href={`/post/${post.data.slug}`}>
-              <div class="relative overflow-hidden ">
-                {/* <img
+			<div class='grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 lg:gap-14 '>
+				{posts.map((post, index) => (
+					<div class='w-full   text-white rounded-md border border-gray-600 dark:bg-dark dark:border-gray-600 transform-gpu scale-100 hover:scale-[1.02] active:scale-[0.97] hover:cursor-pointer transition duration-100 animate-shadow'>
+						<a href={`/post/${post.data.slug}`}>
+							<div class='relative overflow-hidden '>
+								{/* <img
 									src='https://www.w3schools.com/howto/img_avatar2.png'
 									alt='Avatar'
 									class='object-cover w-full h-full'
 								/> */}
-                <img src={post.data.featuredImage} />
+								<img src={post.data.featuredImage} />
 
-                <div class="absolute w-full py-2 bottom-0 inset-x-0  text-white text-s text-center leading-4">
-                  <CategoryChip
-                    className="bg-gray-400"
-                    categories={post.data.tags}
-                  />
-                </div>
-              </div>
-              {/* <div className='relative'>
+								<div class='absolute w-full py-2 bottom-0 inset-x-0  text-white text-s text-center leading-4'>
+									<CategoryChip
+										className='bg-gray-400'
+										categories={post.data.tags}
+									/>
+								</div>
+							</div>
+							{/* <div className='relative'>
 								<img src={post.data.featuredImage}></img>
 							</div> */}
-              <div className="p-2 my-3 ml-3">
-                <h1 className="text-xl font-semibold">
-                  {" "}
-                  <Accent>{post.data.title}</Accent>
-                </h1>
-                <p className="mt-5 text-slate-400">
-                  {" "}
-                  <span className="align-middle text-slate-200 lg:text-lg hidden lg:inline">
-                    {moment(post.data.publishedAt).format("MMMM DD, YYYY")}
-                  </span>
-                </p>
-                <p className="mt-5 text-slate-400">{post.data.excerpt}</p>
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+							<div className='p-2 my-3 ml-3'>
+								<h1 className='text-xl font-semibold'>
+									{" "}
+									<Accent>{post.data.title}</Accent>
+								</h1>
+								<p className='mt-5 text-slate-400'>
+									{" "}
+									<span className='align-middle text-slate-200 lg:text-lg hidden lg:inline'>
+										{moment(post.data.publishedAt).format("MMMM DD, YYYY")}
+									</span>
+								</p>
+								<p className='mt-5 text-slate-400'>{post.data.excerpt}</p>
+							</div>
+						</a>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
 
 // {
 /* <div className="container mx-auto sm:px-0 xs:px-0 lg:px-5 mb-8 bg-gradient-to-tr">
@@ -102,4 +96,4 @@ const BlogScreen = ({ posts }) => {
 </div> */
 // }
 
-export default BlogScreen;
+export default BlogScreen
