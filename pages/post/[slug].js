9b23c0bcb6bccc0ae6b1de897185cdcaa0fd796e
Bojan Jagetic <jageticb@yahoo.com>
2022-01-29 23:43:46 +0100

    syntax highlighter imported and mapped component, using prism instead of hltjs

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,67 +1,78 @@
-import React from "react"
-import { serialize } from "next-mdx-remote/serialize"
-import { MDXRemote } from "next-mdx-remote"
-import fs from "fs"
-import path from "path"
-import matter from "gray-matter"
-import SyntaxHighlighter from "react-syntax-highlighter"
-import { PostDetail, PostBody } from "../../components/"
-import Accent from "../../components/custom/Accent"
-import Image from "next/image"
+import React from "react";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+// import SyntaxHighlighter from "react-syntax-highlighter" and  import prism theme
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+import { PostDetail, PostBody } from "../../components/";
+import Accent from "../../components/custom/Accent";
+import Image from "next/image";
 
 const ResponsiveImage = (props) => (
-	<Image alt={props.alt} layout='responsive' {...props} />
-)
+   <Image alt={props.alt} layout='responsive' {...props} />
+);
 const components = {
-	h1: ({ children }) => (
-		<h1 className='text-white text-4xl font-bold my-5'> {children}</h1>
-	),
-	h2: ({ children }) => (
-		<h2 className='text-white text-2xl font-bold my-5'> {children}</h2>
-	),
-	h3: ({ children }) => (
-		<h3 className='text-white text-xl font-bold my-5'> {children}</h3>
-	),
-	Accent: ({ children }) => <Accent>{children}</Accent>,
-	SyntaxHighlighter: ({ children }) => (
-		<SyntaxHighlighter className='text-black'>{children}</SyntaxHighlighter>
-	),
-	img: ResponsiveImage,
-}
+   h1: ({ children }) => (
+      <h1 className='text-white text-4xl font-bold my-5'> {children}</h1>
+   ),
+   h2: ({ children }) => (
+      <h2 className='text-white text-2xl font-bold my-5'> {children}</h2>
+   ),
+   h3: ({ children }) => (
+      <h3 className='text-white text-xl font-bold my-5'> {children}</h3>
+   ),
+   Accent: ({ children }) => <Accent>{children}</Accent>,
+   img: ResponsiveImage,
+   p: ({ children }) => <p className='my-5'>{children}</p>,
 
-import { Categories, PostWidget } from "../../components"
+   code: ({ children }) => (
+      <SyntaxHighlighter
+         language='jsx'
+         style={a11yDark}
+         showLineNumbers={true}
+         showInlineLineNumbers={true}>
+         {children}
+      </SyntaxHighlighter>
+   ),
+};
+
+import { Categories, PostWidget } from "../../components";
 
 const PostDetails = ({ data, mdxSource }) => {
-	return (
-		<div className='container mx-auto sm:mt-15 lg:mt-5 sm:mt-10 px-3 lg:rounded-lg p-0 lg:p-0 text-slate-400'>
-			<div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-				<div className='col-span-1 lg:col-span-8'>
-					{/* <PostDetail post={data} /> */}
+   return (
+      <div className='container mx-auto sm:mt-15 lg:mt-5 sm:mt-10 px-3 lg:rounded-lg p-0 lg:p-0 text-slate-400'>
+         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+            <div className='col-span-1 lg:col-span-8'>
+               {/* <PostDetail post={data} /> */}
 
-					<MDXRemote {...mdxSource} components={{ ...components }} lazy />
+               <MDXRemote {...mdxSource} components={{ ...components }} lazy />
 
-					{/* <Author author={post.author} /> */}
-					{/* <CommentsForm slug={post.slug} /> */}
-					{/* <Comments slug={post.slug} /> */}
-				</div>
-				<div className='col-span-1 lg:col-span-4'>
-					<div className='relative top-8'>
-						<PostWidget
-						// slug={post.slug}
-						// categories={post.categories.map((category) => category.slug)}
-						/>
-						{/* <Categories /> */}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-	// return (
+               {/* <Author author={post.author} /> */}
+               {/* <CommentsForm slug={post.slug} /> */}
+               {/* <Comments slug={post.slug} /> */}
+            </div>
+            <div className='col-span-1 lg:col-span-4'>
+               <div className='relative top-8'>
+                  <PostWidget
+                  // slug={post.slug}
+                  // categories={post.categories.map((category) => category.slug)}
+                  />
+                  {/* <Categories /> */}
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+   // return (
 
-	// );
-}
+   // );
+};
 
-export default PostDetails
+export default PostDetails;
 
 // export async function getStaticProps({ params }) {
 //   const data = await getPostDetails(params.slug);
@@ -83,32 +94,32 @@ export default PostDetails
 // }
 
 export const getStaticProps = async ({ params: { slug } }) => {
-	const markdownWithMeta = fs.readFileSync(
-		path.join("posts/blog", slug + ".mdx"),
-		"utf-8"
-	)
+   const markdownWithMeta = fs.readFileSync(
+      path.join("posts/blog", slug + ".mdx"),
+      "utf-8"
+   );
 
-	let { content, data } = matter(markdownWithMeta)
-	data = { ...data, slug }
-	const mdxSource = await serialize(content)
+   let { content, data } = matter(markdownWithMeta);
+   data = { ...data, slug };
+   const mdxSource = await serialize(content);
 
-	return {
-		props: {
-			data,
-			mdxSource,
-		},
-	}
-}
+   return {
+      props: {
+         data,
+         mdxSource,
+      },
+   };
+};
 export const getStaticPaths = async () => {
-	const files = fs.readdirSync(path.join("posts/blog"))
-	const paths = files.map((filename) => ({
-		params: {
-			slug: filename.replace(".mdx", ""),
-		},
-	}))
+   const files = fs.readdirSync(path.join("posts/blog"));
+   const paths = files.map((filename) => ({
+      params: {
+         slug: filename.replace(".mdx", ""),
+      },
+   }));
 
-	return {
-		paths,
-		fallback: false,
-	}
-}
+   return {
+      paths,
+      fallback: false,
+   };
+};
