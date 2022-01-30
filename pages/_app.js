b646c9294da08513b3f55a3337c6609da0e52887
Bojan Jagetic <jageticb@yahoo.com>
2022-01-30 03:10:45 +0100

    mdx files added missing props

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { MDXProvider } from "@mdx-js/react";
+
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
-import MdxComponents from "../components/MdxComponents";
-const components = { MdxComponents };
 
 function IguanaDevelopmentTech({ Component, pageProps }) {
    return (
