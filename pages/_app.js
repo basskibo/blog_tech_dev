bb3343cbd42bffc24474e5b03b21079ce5036793
Bojan Jagetic <jageticb@yahoo.com>
2022-06-17 00:51:08 +0200

    dinamic tab name with nextHead

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from "react"
-import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-import { Layout } from "../components"
-import "tailwindcss/tailwind.css"
-import "../styles/globals.scss"
-import * as ga from "../lib/analytics"
-import { motion, MotionConfig, AnimatePresence } from "framer-motion"
+import { Layout } from "../components";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+import * as ga from "../lib/analytics";
+import { motion, MotionConfig, AnimatePresence } from "framer-motion";
+
+import NextHead from "next/head";
 
 function IguanaDevelopmentTech({ Component, pageProps }) {
-	const router = useRouter()
+   const router = useRouter();
+   useEffect(() => {
+      const handleRouteChange = (url) => {
+         console.log(url);
+         ga.pageview(url);
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+         router.events.off("routeChangeComplete", handleRouteChange);
+      };
+   }, [router.events]);
 
-	useEffect(() => {
-		const handleRouteChange = (url) => {
-			console.log(url)
-			ga.pageview(url)
-		}
-		router.events.on("routeChangeComplete", handleRouteChange)
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange)
-		}
-	}, [router.events])
-
-	return (
-		<Layout>		
-			<Component {...pageProps} />{" "}
-		</Layout>
-	)
+   return (
+      <>
+         <NextHead>
+            <title>
+               {pageProps.data
+                  ? `BJ | ${pageProps.data.title}`
+                  : "Bojan Jagetic"}
+            </title>
+         </NextHead>
+         <Layout>
+            <Component {...pageProps} />
+         </Layout>
+      </>
+   );
 }
 
-export default IguanaDevelopmentTech
+export default IguanaDevelopmentTech;
