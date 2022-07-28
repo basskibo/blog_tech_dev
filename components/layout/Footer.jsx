7bb68302a00f119814ff09fad93dee749d509095
Bojan Jagetic <jagetic.bojan@gmail.com>
2022-07-28 19:09:19 +0200

    github card UI changed as on github

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -58,8 +58,6 @@ const Footer = () => {
                </a>
             </p>
 
-            <NewsLetter />
-
             {/* <span className='inline-flex sm:ml-auto mx-5 sm:mt-0 mt-4 justify-center sm:justify-start'>
 					<SpotifyCard />
 				</span> */}
