a92485f0c44129d99bc78259128c0d1b4e640cbd
Bojan Jagetic <jagetic.bojan@gmail.com>
2022-07-28 19:11:20 +0200

    motion animation on load disabled

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -19,83 +19,83 @@ const Landing = () => {
       // <div className='bg-gradient-to-r from-amber-800 to-lime-900 '>
       <div className=''>
          <div className=' text-slate-200 md:px-10 px-5  hero-bg '>
-            <MotionComponent>
-               <div
-                  className='grid place-items-left place-content-center gap-1 h-screen
+            {/* <MotionComponent> */}
+            <div
+               className='grid place-items-left place-content-center gap-1 h-screen
 					mx-auto lg:max-w-4xl sm:max-w-xl'>
-                  <h1 className=' text-3xl xs:text-2xl lg:text-5xl  font-bold mb-2'>
-                     Hello there,
-                     <br />
-                     <span className='text-3xl sm:text-3xl lg:text-6xl '>
-                        My name is <Accent>Bojan</Accent>
-                     </span>
-                  </h1>
+               <h1 className=' text-3xl xs:text-2xl lg:text-5xl  font-bold mb-2'>
+                  Hello there,
+                  <br />
+                  <span className='text-3xl sm:text-3xl lg:text-6xl '>
+                     My name is <Accent>Bojan</Accent>
+                  </span>
+               </h1>
 
-                  <div className='lg:mt-5'>
-                     <p className='font-bold mb-1 lg:hidden text-md xs:text-md'>
-                        Writing code is my obsession as well as constant
-                        self-improvement, I have developed many web applications
-                        and platforms from scratch in my spare time (when
-                        I&apos;m not coding). Accustomed to using extensive tech
-                        stack because I love challenging and complex projects.
-                     </p>
-                     <p className='font-bold mb-1  hidden lg:block text-md'>
-                        Writing code is my obsession as well as constant
-                        self-improvement, I have developed many web applications
-                        and platforms from scratch in my spare time (when
-                        I&apos;m not coding). Accustomed to using extensive tech
-                        stack because I love challenging and complex projects.
-                     </p>
-                  </div>
-                  <div className='mt-7  lg:mt-10  xl:grid-cols-2 sm:grid-cols-1 gap-2 text-sm sm:text-md xs:text-sm lg:text-md'>
-                     <ActiveLink
-                        href='/routes/blog'
-                        name='blog'
-                        id='rb_button'
-                        className='mt-5 px-5 lg:px-10 rounded-md  p-3 shadow-md  shadow-neutral-700
-                  hover:cursor-pointer text-center text-white  border border-lime-500 hover:text-lime-600 hover:shadow-neutral-500'>
-                        Read blog
-                     </ActiveLink>
-                     <ActiveLink
-                        href='/routes/aboutme'
-                        id='am_button'
-                        className='mt-5 px-5 lg:px-10 rounded-md  p-3 shadow-md  shadow-neutral-700
-							hover:cursor-pointer text-center text-white  border border-lime-500 hover:text-lime-600 hover:shadow-neutral-500'>
-                        See my bio
-                     </ActiveLink>
-                  </div>
-                  <div className='mt-7 lg:mt-10 flex gap-x-4'>
-                     <a
-                        id='lnk-linkedin-button'
-                        className='inline-flex text-sm text-slate-300 hover:underline link link-underline link-underline-black hover:cursor-ne-resize'
-                        href='https://linkedin.com/in/bojan-jagetic-972203106'
-                        target='_blank'
-                        rel='noreferrer'>
-                        <SiLinkedin className='mr-1 h-4 w-4' />
-                        <span className='hidden lg:block'>Bojan Jagetic</span>
-                        <span className='lg:hidden '>B.Jagetic</span>
-                     </a>
-                     <a
-                        id='lnk-github-button'
-                        className='inline-flex  text-sm text-slate-300 hover:underline link link-underline link-underline-black
-							 hover:cursor-ne-resize'
-                        href='https://github.com/basskibo/'
-                        target='_blank'
-                        rel='noreferrer'>
-                        <SiGithub className='mr-1 h-4 w-4' /> basskibo
-                     </a>
-                     <a
-                        id='lnk-twitter-button'
-                        className='inline-flex text-sm text-slate-300 hover:underline link link-underline link-underline-black hover:cursor-ne-resize'
-                        href='https://twitter.com/basskibo1'
-                        target='_blank'
-                        rel='noreferrer'>
-                        <SiTwitter className='mr-1 h-4 w-4 ' />
-                        <span className=' lg:block'>basskibo1</span>
-                     </a>
-                  </div>
+               <div className='lg:mt-5'>
+                  <p className='font-bold mb-1 lg:hidden text-md xs:text-md'>
+                     Writing code is my obsession as well as constant
+                     self-improvement, I have developed many web applications
+                     and platforms from scratch in my spare time (when I&apos;m
+                     not coding). Accustomed to using extensive tech stack
+                     because I love challenging and complex projects.
+                  </p>
+                  <p className='font-bold mb-1  hidden lg:block text-md'>
+                     Writing code is my obsession as well as constant
+                     self-improvement, I have developed many web applications
+                     and platforms from scratch in my spare time (when I&apos;m
+                     not coding). Accustomed to using extensive tech stack
+                     because I love challenging and complex projects.
+                  </p>
                </div>
-            </MotionComponent>
+               <div className='mt-7  lg:mt-10  xl:grid-cols-2 sm:grid-cols-1 gap-2 text-sm sm:text-md xs:text-sm lg:text-md'>
+                  <ActiveLink
+                     href='/routes/blog'
+                     name='blog'
+                     id='rb_button'
+                     className='mt-5 px-5 lg:px-10 rounded-md  p-3 shadow-md  shadow-neutral-700
+                  hover:cursor-pointer text-center text-white  border border-lime-500 hover:text-lime-600 hover:shadow-neutral-500'>
+                     Read blog
+                  </ActiveLink>
+                  <ActiveLink
+                     href='/routes/aboutme'
+                     id='am_button'
+                     className='mt-5 px-5 lg:px-10 rounded-md  p-3 shadow-md  shadow-neutral-700
+							hover:cursor-pointer text-center text-white  border border-lime-500 hover:text-lime-600 hover:shadow-neutral-500'>
+                     See my bio
+                  </ActiveLink>
+               </div>
+               <div className='mt-7 lg:mt-10 flex gap-x-4'>
+                  <a
+                     id='lnk-linkedin-button'
+                     className='inline-flex text-sm text-slate-300 hover:underline link link-underline link-underline-black hover:cursor-ne-resize'
+                     href='https://linkedin.com/in/bojan-jagetic-972203106'
+                     target='_blank'
+                     rel='noreferrer'>
+                     <SiLinkedin className='mr-1 h-4 w-4' />
+                     <span className='hidden lg:block'>Bojan Jagetic</span>
+                     <span className='lg:hidden '>B.Jagetic</span>
+                  </a>
+                  <a
+                     id='lnk-github-button'
+                     className='inline-flex  text-sm text-slate-300 hover:underline link link-underline link-underline-black
+							 hover:cursor-ne-resize'
+                     href='https://github.com/basskibo/'
+                     target='_blank'
+                     rel='noreferrer'>
+                     <SiGithub className='mr-1 h-4 w-4' /> basskibo
+                  </a>
+                  <a
+                     id='lnk-twitter-button'
+                     className='inline-flex text-sm text-slate-300 hover:underline link link-underline link-underline-black hover:cursor-ne-resize'
+                     href='https://twitter.com/basskibo1'
+                     target='_blank'
+                     rel='noreferrer'>
+                     <SiTwitter className='mr-1 h-4 w-4 ' />
+                     <span className=' lg:block'>basskibo1</span>
+                  </a>
+               </div>
+            </div>
+            {/* </MotionComponent> */}
          </div>
          <AnchorLink
             id='btn-anchor-linkg'
--- pages/routes/libaries.js
+++ pages/routes/libaries.js
@@ -10,32 +10,32 @@ import MotionComponent from "../../components/custom/MotionComponent";
 
 const Libaries = ({ libaries }) => {
    return (
-      <MotionComponent>
-         <div className='container mx-auto lg:my-14 my-5 px-5 sm:px-2 xs:px-3 lg:px-5 bg-gradient-to-tr text-slate-400'>
-            <div className='my-8'>
-               <h1 className='mb-5'>
-                  <Accent className='font-extrabold text-5xl'>Libary</Accent>
-               </h1>
-               <p className='display-4'>
-                  Here you can find some of my simple libaries and repositories
-                  which are open source so feel free to use it in your projects.
-               </p>
-               <input
-                  className='w-full my-3 bg-neutral-900  px-3 py-2 rounded-md
+      // <MotionComponent>
+      <div className='container mx-auto lg:my-14 my-5 px-5 sm:px-2 xs:px-3 lg:px-5 bg-gradient-to-tr text-slate-400'>
+         <div className='my-8'>
+            <h1 className='mb-5'>
+               <Accent className='font-extrabold text-5xl'>Libary</Accent>
+            </h1>
+            <p className='display-4'>
+               Here you can find some of my simple libaries and repositories
+               which are open source so feel free to use it in your projects.
+            </p>
+            <input
+               className='w-full my-3 bg-neutral-900  px-3 py-2 rounded-md
 					border border-slate-600 focus:border-lime-600'
-                  placeholder='Search...'></input>
-            </div>
-            <div className='grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 lg:gap-7'>
-               {libaries.map((libary, index) => (
-                  // <PostCard className='' key={post.props.data.slug} post={post.props} />
-                  <LibaryCard
-                     libary={libary.props.data}
-                     key={libary.props.data.slug}
-                  />
-               ))}
-            </div>
+               placeholder='Search...'></input>
          </div>
-      </MotionComponent>
+         <div className='grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 lg:gap-7'>
+            {libaries.map((libary, index) => (
+               // <PostCard className='' key={post.props.data.slug} post={post.props} />
+               <LibaryCard
+                  libary={libary.props.data}
+                  key={libary.props.data.slug}
+               />
+            ))}
+         </div>
+      </div>
+      // </MotionComponent>
    );
 };
 export default Libaries;
