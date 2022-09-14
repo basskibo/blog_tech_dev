2ab24c2d870b8a59ffc6ed328b8a28ea088f1105
Bojan Jagetic <jageticb@yahoo.com>
2022-09-15 01:13:51 +0200

    UI changes for libary and posts metadata

--- components/ReadTime.jsx
+++ components/ReadTime.jsx
@@ -1,19 +1,19 @@
-import React from "react"
-import Accent from "./custom/Accent"
-const readingTime = require("reading-time")
-import { IoTimeOutline } from "react-icons/io5"
-import cslx from "clsx"
+import React from "react";
+import Accent from "./custom/Accent";
+const readingTime = require("reading-time");
+import { IoTimeOutline } from "react-icons/io5";
+import cslx from "clsx";
 
 const ReadTime = ({ children, className }) => {
-	const stats = readingTime(JSON.stringify(children[1]))
-	return (
-		<p className={cslx(className, "inline-flex mt-1")}>
-			<IoTimeOutline className='h-full mt-0.5' />{" "}
-			<span className='ml-2'>
-				<Accent className='text-md'>{stats.text}</Accent>
-			</span>
-		</p>
-	)
-}
+   const stats = readingTime(JSON.stringify(children[1]));
+   return (
+      <p className={cslx(className, "inline-flex mt-1")}>
+         <IoTimeOutline className='h-full mt-0.7 text-lg' />{" "}
+         <span className='ml-2'>
+            <Accent className='text-md'>{stats.text}</Accent>
+         </span>
+      </p>
+   );
+};
 
-export default ReadTime
+export default ReadTime;
