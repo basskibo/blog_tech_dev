7ccaa73b8e17155e180ab9bb104dfc1c94db3e5e
basskibo <jagetic.bojan@gmail.com>
2022-10-16 22:17:07 +0200

    post in preparation condition with clsx

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -62,12 +62,12 @@ const Header = ({ href }) => {
                            <div className='flex-shrink-0 flex items-center'>
                               <a href='/'>
                                  <img
-                                    className='block lg:hidden h-8 w-auto'
+                                    className='block lg:hidden h-8 w-auto '
                                     src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
                                     alt='Workflow'
                                  />
                                  <img
-                                    className='hidden lg:block h-8 w-auto'
+                                    className='hidden lg:block h-8 w-auto animate-pulse'
                                     src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
                                     alt='Workflow'
                                  />
