e29b7cb7841479879df785a5adc766b33465b55e
Bojan <jagetic.bojan@gmail.com>
2022-10-26 15:27:48 +0200

    feat: newslatter dialog added and logic, libary cards fixed

--- components/LibaryCard.jsx
+++ components/LibaryCard.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import constants from "../lib/constants";
 import moment from "moment";
 import clsx from "clsx";
+import { SiGithub } from "react-icons/si";
 
 const LibaryCard = ({ libary }) => {
    return (
       <div
          key={libary.slug}
-         className={clsx(libary.inPreparation ? "text-slate-600 pointer-events-none bg-blend-hard-light bg-neutral-800 " :
+         className={clsx(libary.inPreparation ? "text-slate-600 pointer-events-none bg-neutral-800 " :
          "text-white transform-gpu scale-100 hover:scale-[1.03] active:scale-[0.97] hover:cursor-pointer transition duration-100 animate-shadow",
          "w-full  py-3 px-2 text-white  shadow-lg shadow-neutral-800 rounded-md border border-gray-600 dark:bg-dark dark:border-gray-600 transform-gpu scale-100 hover:scale-[1.03] active:scale-[0.97] hover:cursor-pointer transition duration-100 animate-shadow overflow-hidden")}
          >
          <a href={`/libary/${libary.slug}`}>
-         <div  className={clsx(libary.inPreparation ? " bg-gradient-to-r from-lime-500 to-teal-600 origin-top float-right mt-9 mr-9 w-52 text-center text-slate-200" : "block-hidden")} style={{transform:"translateX(50%) rotate(45deg)"}} >
-                    <div className="text-sm p-1">Comming soon</div>
-                </div>
+            {libary.inPreparation ?  
+            <div  className=" bg-gradient-to-r from-lime-700 to-teal-700 origin-top float-right mt-6 mr-6 w-36 text-center text-slate-200" 
+            style={{transform:"translateX(54%) rotate(50deg)"}} >
+               <div className="text-xs py-1">Comming soon</div>
+            </div> : <div  className="origin-top float-right mt-4 mr-2 w-8 text-right text-slate-200 " > 
+            <SiGithub className="mr-0 pr-0 text-xl text-right content-end "></SiGithub>
+            </div>}
+           
+
             <div className='px-3 my-3 '>
-               <h1 className='text-lg font-semibold'> {libary.title}</h1>
                <div className='mt-3 text-slate-400 '>
-                  {" "}
-                  <p className='align-middle font-bold text-slate-400 mt-1'>
+               <h1 className='text-lg font-bold  whitespace-normal break-normal'> {libary.title}</h1>
+
+                  <p className='align-middle  text-slate-400 mt-1'>
                      {moment(libary.publishedAt).format("MMMM DD, YYYY")}
                   </p>
                   <div className=' text-slate-400 text-sm flex mt-3'>
--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {useState, Fragment } from "react";
 import Accent from "./custom/Accent";
+import { Dialog, Transition } from '@headlessui/react'
 
 const NewsLetter = () => {
+   let [isOpen, setIsOpen] = useState(false);
+   const [email, setemail] = useState("");
+   const [validationError, setValidationError ] = useState(false);
+
+   function closeModal() {
+      setemail("")
+      setIsOpen(false)
+   }
+ 
+   function openModal() {
+      let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+      if(regex.test(email)){
+         setIsOpen(true)
+         setValidationError(false)
+          console.log("mejl je dobar")
+      }else{
+         console.log("mejl nije dobar")
+         setValidationError(true)
+      }
+   }
+
+   function handleChange(e) {
+      console.log(e.target.value)
+      setemail(e.target.value)
+      // setIsOpen(true)
+    }
+ 
+
    return (
       <div className=' '>
          <div className=' xl:w-2/3 md:w-3/3 sm:w-3/3 mx-auto relative border bg-neutral-900 border-neutral-700 grid grid-cols-1 lg:grid-cols-2 gap-10  p-10 lg:p-[60px] rounded-2xl'>
@@ -25,30 +54,86 @@ const NewsLetter = () => {
                      className='px-4 w-full bg-neutral-900 rounded-md border-gray-300 ring ring-gray-500 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
                   /> */}
                   <input
-                     className=' w-4/4 my-3 bg-neutral-900  px-3 py-2 rounded-md
+                     className=' w-4/4 my-3 bg-neutral-900 text-white  px-3 py-2 rounded-md
 					border border-slate-600 focus:border-lime-600 flex-grow focus:outline-none cursor-text'
                      placeholder='Enter your email'
-                     value={""}
-                     type='search'
+                     value={email}
+                     onChange={handleChange}
+                     type='email'
                      // onChange={handleSearchChange}
                   />
                   <button
+                     type="button"
+                     onClick={openModal}
+                     disabled={!email}
                      className=' ml-2 p-2 w-4/4 my-3 rounded-md  shadow-md  shadow-neutral-700
                   hover:cursor-pointer text-center text-white  border border-lime-500 hover:text-lime-600 hover:shadow-neutral-500'>
                      Subscribe
                   </button>
                </div>
 
-               <p className='text-white mt-2 text-xs'>
-                  We care about your data in our
-                  <a href='#' className='underline'>
-                     {" "}
-                     privacy policy
-                  </a>
-                  .
-               </p>
+                  {validationError ? <p className='text-red-700 mt-2 font-semibold  text-md'>
+                  Email format is wrong, please try again
+               </p> :<></>}
+              
             </div>
          </div>
+         <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md	" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-neutral-900  border border-neutral-700 p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-white"
+                  >
+                    Subscription successful
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-slate-400">
+                      Your subscription has been successfully submitted for email <b>{email}</b>. We strive to fight against data and privacy abuse, so please confirm your email. 
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                     <span className="bg-gradient-to-r p-[2px]  from-[#7928ca] to-[#ff0080]">
+                     <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-neutral-900 text-sm text-white p-3"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                     </span>
+                 
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
       </div>
    );
 };
