3b5e199910018e373891f7fb0f8e2c1c0c7b3ac1
Bojan <jagetic.bojan@gmail.com>
2022-11-01 18:02:38 +0100

    fix: footer image changed to logo

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -31,20 +31,21 @@ const Footer = () => {
    return (
       <footer className='text-gray-400 bg-neutral-900 body-font border-t border-neutral-800 '>
          <div className='container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col'>
-            <a className='flex title-font font-medium items-center md:justify-start justify-center text-white'>
-               <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-                  viewBox='0 0 24 24'>
-                  <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
-               </svg>
-               <span className='ml-3 text-xl'>Tailblocks</span>
-            </a>
+             <div className='flex-shrink-0 flex items-center'>
+                 <a href='/'>
+                     <img
+                         className='block lg:hidden h-8 w-auto '
+                         src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821240/igutech/logo/logo_jibobs.webp'
+                         alt='Jagetic'
+                     />
+                     <img
+                         className='hidden lg:block h-8 w-auto '
+                         src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821292/igutech/logo/logo-no-background_dbpm3q.svg'
+                         // src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+                         alt='Jagetic'
+                     />
+                 </a>
+             </div>
             <p className='text-sm text-gray-400 sm:ml-4 sm:pl-4  sm:border-gray-800 sm:py-2 sm:mt-0 mt-4'>
                © 2022 Bojan Jagetic —
                <a
