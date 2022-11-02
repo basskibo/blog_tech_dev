ec7975d6f6789a08de337863e40ad1e8d0d6c657
Bojan <jagetic.bojan@gmail.com>
2022-11-02 18:05:00 +0100

    feat: eslint added, changing code style

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,45 +1,11 @@
-import React from "react"
+import React from 'react'
 // import { getPosts, getPostsWithCategory } from "../../services"
 // import { getAllCategories, getCategories } from "../../services"
-import { Categories, Pagination, PostCard } from "../../components"
+// import { Categories, Pagination, PostCard } from '../../components'
 
 // const PostDetails = ({ post }) => {
 const PostDetails = () => {
-	return <div></div>
-	//<div className='container mx-auto lg:px-10 sm:px-0 xs:px-0  mb-8 '>
-	// 	<div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-	// 		<div className='lg:col-span-8 col-span-1'>
-	// 			{post.map((p, index) => (
-	// 				<PostCard post={p} key={p.title} />
-	// 			))}
-	// 			<Pagination pageNumber={1} numberOfPosts={post.length} numberOfPages={3} />
-	// 		</div>
-	// 		<div className='lg:col-span-4 col-span-1'>
-	// 			<div className='lg:sticky relative top-8'>
-	// 				<PostWidget />
-	// 				<Categories />
-	// 			</div>
-	// 		</div>
-	// 	</div>
-	// </div>
-	// )
+  return <div></div>
 }
 
 export default PostDetails
-
-// export async function getStaticProps({ params }) {
-// 	const data = await getPostsWithCategory(params.slug)
-// 	return {
-// 		props: { post: data },
-// 	}
-// }
-
-// export async function getStaticPaths() {
-// 	const categories = await getCategories()
-// 	const statics = {
-// 		paths: categories.map(({ slug }) => ({ params: { slug } })),
-// 		fallback: false,
-// 	}
-// 	console.log(JSON.stringify(statics))
-// 	return statics
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import Accent from "../components/custom/Accent";
-import Landing from "../components/Landing";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import Head from 'next/head'
+import Landing from '../components/Landing'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-export default function Home({ posts }) {
-   // const [numberOfPages, setnumberOfPages] = useState(0)
-   // const postNum = posts.length
-   // const [numberOfPosts, setNumberOfPosts] = useState(postNum)
-   // const [pageNumber, setpageNumber] = useState(1)
-   // // console.log(posts);
-   // useEffect(() => {
-   // 	const offset = (pageNumber - 1) * numberPerPage + 1
-   // 	const calculatedPages = Math.ceil(posts.length / numberPerPage)
-   // 	setnumberOfPages(calculatedPages)
-   // 	setpageNumber(offset)
-   // }, [])
+export default function Home () {
+  // const [numberOfPages, setnumberOfPages] = useState(0)
+  // const postNum = posts.length
+  // const [numberOfPosts, setNumberOfPosts] = useState(postNum)
+  // const [pageNumber, setpageNumber] = useState(1)
+  // // console.log(posts);
+  // useEffect(() => {
+  // const offset = (pageNumber - 1) * numberPerPage + 1
+  // const calculatedPages = Math.ceil(posts.length / numberPerPage)
+  // setnumberOfPages(calculatedPages)
+  // setpageNumber(offset)
+  // }, [])
 
-   return (
+  return (
       // <div className='container mx-auto sm:px-5 sm:mx-5 xs:px-5'>
       <div>
          <Head>
@@ -62,25 +59,25 @@ export default function Home({ posts }) {
          <Landing />
          {/* </div> */}
       </div>
-   );
+  )
 }
 
 export const getStaticProps = async () => {
-   const files = fs.readdirSync(path.join("posts/blog"));
-   const posts = files.map((filename) => {
-      const markdownWithMeta = fs.readFileSync(
-         path.join("posts/blog", filename),
-         "utf-8"
-      );
-      const { data: postData } = matter(markdownWithMeta);
-      const data = { ...postData, slug: filename.split(".")[0] };
-      return {
-         data,
-      };
-   });
-   return {
-      props: {
-         posts,
-      },
-   };
-};
+  const files = fs.readdirSync(path.join('posts/blog'))
+  const posts = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts/blog', filename),
+      'utf-8'
+    )
+    const { data: postData } = matter(markdownWithMeta)
+    const data = { ...postData, slug: filename.split('.')[0] }
+    return {
+      data
+    }
+  })
+  return {
+    props: {
+      posts
+    }
+  }
+}
