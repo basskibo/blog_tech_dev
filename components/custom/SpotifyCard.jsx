ee938e27d58bc2544f5787f2501e7363a79091cb
Bojan <jagetic.bojan@gmail.com>
2022-11-09 00:22:20 +0100

    feat: SWR included for spotify route

--- components/custom/SpotifyCard.jsx
+++ components/custom/SpotifyCard.jsx
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import axios from 'axios'
 import { SiSpotify } from 'react-icons/si'
 import Accent from './Accent'
+import useSWR from 'swr'
 
-const fetchCurent = async () => {
-  const url = '/api/spotify'
-  const result = await axios(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  return result.data
-}
+const fetcher = url => axios.get(url).then(res => res.data)
 
 const SpotifyCard = () => {
-  const [song, setSong] = useState({})
-  // const [seconds, setSeconds] = useState(1000)
-
-  useEffect(() => {
-    // const interval = setInterval(() => {
-    fetchCurent().then((result) => setSong(result))
-    // setSeconds(15000);
-    // }, seconds);
-    // return () => clearInterval(interval);
-  }, [])
+  const { data, error } = useSWR('api/spotify', fetcher)
+  console.log(data)
+  if (error) {
+    // TODO Make notification dialog popup
+    console.log(error)
+  }
 
   return (
       <div className='text-slate-400 bg-neutral-900 border border-neutral-600 rounded-lg w-7/8'>
@@ -32,18 +20,18 @@ const SpotifyCard = () => {
             target='_blank'
             rel='noreferrer'
             href={
-               song?.isPlaying
-                 ? song.songUrl
+               data?.isPlaying
+                 ? data.songUrl
                  : 'https://open.spotify.com/user/4g1ztvqi3z5mf0uqx87bz9exk'
             }
             className='relative flex  items-center space-x-2 py-4 pl-2 transition-shadow hover:shadow-md'>
             <div className='w-18'>
-               {song?.isPlaying
+               {data?.isPlaying
                  ? (
                   <img
                      className='w-12 shadow-sm'
-                     src={song?.albumImageUrl}
-                     alt={song?.album}
+                     src={data?.albumImageUrl}
+                     alt={data?.album}
                   />
                    )
                  : (
@@ -53,26 +41,26 @@ const SpotifyCard = () => {
 
             <div className='flex-1'>
                <p className='component text-xs'>
-                  {song?.isPlaying ? 'Currently listening:' : ''}
+                  {data?.isPlaying ? 'Currently listening:' : ''}
                </p>
                <p className='component font-bold text-md w-48 lg:w-72 truncate'>
-                  {song?.isPlaying
+                  {data?.isPlaying
                     ? (
-                     <Accent>{song.title}</Accent>
+                     <Accent>{data.title}</Accent>
                       )
                     : (
                         'Currently not listening'
                       )}
                </p>
                <p className='font-dark text-sm text-semibold'>
-                  {song?.isPlaying ? `Artist: ${song.artist}` : 'Spotify'}
+                  {data?.isPlaying ? `Artist: ${data.artist}` : 'Spotify'}
                </p>
                <p className='font-dark text-xs text-semibold truncate'>
-                  {song?.isPlaying ? `Album: ${song.album}` : ''}
+                  {data?.isPlaying ? `Album: ${data.album}` : ''}
                </p>
             </div>
             <div className='absolute right-1.5 top-1.5'>
-               {song?.isPlaying
+               {data?.isPlaying
                  ? (
                   <SiSpotify size={20} color={'#1ED760'} />
                    )
