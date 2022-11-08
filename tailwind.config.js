e4a93d9d13233531c7e7d3bbf3bbaa13c79249f5
Bojan <jagetic.bojan@gmail.com>
2022-11-09 00:08:07 +0100

    fix: configurations fixed by eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"hero-pattern": "url('../public/images/hero.svg')",
-			},
-		},
-	},
-	//   theme: {
-	//     screens: {
-	//        sm: { min: "640px", max: "767px" },
-	//        // => @media (min-width: 640px and max-width: 767px) { ... }
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('../public/images/hero.svg')"
+      }
+    }
+  },
+  //   theme: {
+  //     screens: {
+  //        sm: { min: "640px", max: "767px" },
+  //        // => @media (min-width: 640px and max-width: 767px) { ... }
 
-	//        md: { min: "768px", max: "1023px" },
-	//        // => @media (min-width: 768px and max-width: 1023px) { ... }
+  //        md: { min: "768px", max: "1023px" },
+  //        // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-	//        lg: { min: "1024px", max: "1279px" },
-	//        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+  //        lg: { min: "1024px", max: "1279px" },
+  //        // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-	//        xl: { min: "1280px", max: "1535px" },
-	//        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+  //        xl: { min: "1280px", max: "1535px" },
+  //        // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-	//        "2xl": { min: "1536px" },
-	//        // => @media (min-width: 1536px) { ... }
-	//     },
-	//  },
-	plugins: [require("@tailwindcss/typography"),     require('autoprefixer')],
+  //        "2xl": { min: "1536px" },
+  //        // => @media (min-width: 1536px) { ... }
+  //     },
+  //  },
+  plugins: [require('@tailwindcss/typography'), require('autoprefixer')]
 }
