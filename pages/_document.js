26b98f468720415d34b6cd13c2ca9286a31e8845
Bojan Jagetic <jagetic.bojan@gmail.com>
2024-03-13 23:23:15 +0100

    gtag measurment id in env

--- pages/_document.js
+++ pages/_document.js
@@ -2,36 +2,46 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
+  static async getInitialProps (ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-					<script async src="https://www.googletagmanager.com/gtag/js?id=UA-71330880-1"></script>
-					<script>
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments)}
-						gtag('js', new Date());
-
-						gtag('config', 'UA-71330880-1');
-					</script>
-
-					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3181880928286370"
-						crossorigin="anonymous"></script>
-				</Head>
-				<body>
-					<div id={'globalLoader'} >
-					</div>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render () {
+    return (
+         <Html lang="en">
+            <Head>
+               <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_GOOGLE_ANALYTIC_MEASUREMENT_ID}`}
+               />
+               <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', ${JSON.stringify(
+                                   process.env
+                                      .NEXT_GOOGLE_ANALYTIC_MEASUREMENT_ID
+                                )} , {
+                                page_path: window.location.pathname,
+                                });
+                            `
+                  }}
+               />
+			   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3181880928286370"
+     crossorigin="anonymous"></script>
+            </Head>
+            <body>
+               <div id={'globalLoader'} >
+               </div>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+    )
+  }
 }
 
 export default MyDocument
