42207aae3ea8651afe8cabfd0c3ab38516e55b82
Bojan Jagetic <jagetic.bojan@gmail.com>
2024-03-14 13:10:01 +0100

    fix: interview quiz removed until done

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -5,14 +5,14 @@ import ActiveLink from '../custom/ActiveLink'
 // import Image from 'next/image'
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Blog', href: '/routes/blog', current: false },
+	{ name: 'Home', href: '/', current: true },
+	{ name: 'Blog', href: '/routes/blog', current: false },
 
-  // { name: "Projects", href: "/routes/projects", current: false },
-  // { name: "Courses", href: "/routes/courses", current: false },
-  { name: 'Libary', href: '/routes/libaries', current: false },
-  { name: "Interview Quiz", href: "/quiz", current: false },
-  { name: 'About Me', href: '/routes/aboutme', current: false }
+	// { name: "Projects", href: "/routes/projects", current: false },
+	// { name: "Courses", href: "/routes/courses", current: false },
+	{ name: 'Libary', href: '/routes/libaries', current: false },
+	// { name: "Interview Quiz", href: "/quiz", current: false },
+	{ name: 'About Me', href: '/routes/aboutme', current: false }
 ]
 
 // function classNames (...classes) {
@@ -20,70 +20,70 @@ const navigation = [
 // }
 
 const Header = ({ href }) => {
-  return (
-      <div className=' sticky top-0 z-50 bg-neutral-700 backdrop-filter backdrop-blur-lg bg-opacity-30 '>
-         <Disclosure
-            as='nav'
-            className=' border-b border-neutral-500 lg:pb-1 text-white  lg:pt-1'>
-            {({ open }) => (
-               <>
-                  <div className='header-layout max-w-10xl w-full  sm:px-6'>
-                     <div className='relative flex items-center justify-between h-16'>
-                        <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-                           {/* Mobile menu button */}
-                           <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white absolute'>
-                              <span className='sr-only'>Open main menu</span>
-                              {open
-                                ? (
-                                 <XIcon
-                                    className='block h-6 w-6'
-                                    aria-hidden='true'
-                                 />
-                                  )
-                                : (
-                                 <MenuIcon
-                                    className='block h-6 w-6 text-[#ff0080]'
-                                    aria-hidden='true'
-                                 />
-                                  )}
-                           </Disclosure.Button>
-                        </div>
-                        <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-                           <div className='flex-shrink-0 flex items-center'>
-                              <a href='/'>
-                                 <img
-                                    className='block lg:hidden h-8 w-auto '
-                                    src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821240/igutech/logo/logo_jibobs.webp'
-                                    width={30}
-                                    height={30}
-                                    alt='Jagetic'
-                                 />
-                                 <img
-                                    className='hidden lg:block h-8 w-auto animate-pulse'
-                                    src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821292/igutech/logo/logo-no-background_dbpm3q.svg'
-                                    // src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
-                                    alt='Jagetic'
-                                 />
-                              </a>
-                           </div>
-                           <div className='hidden sm:block sm:ml-6'>
-                              <div className='flex space-x-5'>
-                                 {/* <DisclocureButton /> */}
-                                 {navigation.map((item) => (
-                                    <ActiveLink
-                                       key={item.name}
-                                       href={item.href}
-                                       id={ `${item.name}_header_button`}
-                                       data-splitbee-event={`Header clicked ${item.name}_header_button`}
-                                       >
-                                       {item.name}
-                                    </ActiveLink>
-                                 ))}
-                              </div>
-                           </div>
-                        </div>
-                        <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 xs:hidden'>
-                           {/* <div className='text-center hidden lg:inline'>
+	return (
+		<div className=' sticky top-0 z-50 bg-neutral-700 backdrop-filter backdrop-blur-lg bg-opacity-30 '>
+			<Disclosure
+				as='nav'
+				className=' border-b border-neutral-500 lg:pb-1 text-white  lg:pt-1'>
+				{({ open }) => (
+					<>
+						<div className='header-layout max-w-10xl w-full  sm:px-6'>
+							<div className='relative flex items-center justify-between h-16'>
+								<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+									{/* Mobile menu button */}
+									<Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white absolute'>
+										<span className='sr-only'>Open main menu</span>
+										{open
+											? (
+												<XIcon
+													className='block h-6 w-6'
+													aria-hidden='true'
+												/>
+											)
+											: (
+												<MenuIcon
+													className='block h-6 w-6 text-[#ff0080]'
+													aria-hidden='true'
+												/>
+											)}
+									</Disclosure.Button>
+								</div>
+								<div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+									<div className='flex-shrink-0 flex items-center'>
+										<a href='/'>
+											<img
+												className='block lg:hidden h-8 w-auto '
+												src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821240/igutech/logo/logo_jibobs.webp'
+												width={30}
+												height={30}
+												alt='Jagetic'
+											/>
+											<img
+												className='hidden lg:block h-8 w-auto animate-pulse'
+												src='https://res.cloudinary.com/dr1sm5gnj/image/upload/v1666821292/igutech/logo/logo-no-background_dbpm3q.svg'
+												// src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+												alt='Jagetic'
+											/>
+										</a>
+									</div>
+									<div className='hidden sm:block sm:ml-6'>
+										<div className='flex space-x-5'>
+											{/* <DisclocureButton /> */}
+											{navigation.map((item) => (
+												<ActiveLink
+													key={item.name}
+													href={item.href}
+													id={`${item.name}_header_button`}
+													data-splitbee-event={`Header clicked ${item.name}_header_button`}
+												>
+													{item.name}
+												</ActiveLink>
+											))}
+										</div>
+									</div>
+								</div>
+								<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 xs:hidden'>
+									{/* <div className='text-center hidden lg:inline'>
                               <Link href={`/`}>
                                  <a className='inline-flex'>
                                     <span className='h-9 mb-3 flex items-center mt-3 justify-center  font-semibold px-5 border-2 border-lime-800 text-slate-400 hover:bg-sky-800 hover:text-white hover:cursor-pointer transition duration-500 ease-in-out'>
@@ -106,7 +106,7 @@ const Header = ({ href }) => {
                                  </a>
                               </Link>
                            </div> */}
-                           {/*
+									{/*
                            <button
                               type='button'
                               className='bg-gray-800 p-1 rounded-full text-gray-400 ml-2 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
@@ -118,8 +118,8 @@ const Header = ({ href }) => {
                                  aria-hidden='true'
                               />
                            </button> */}
-                           {/* Profile dropdown */}
-                           {/* <Menu as='div' className='ml-3 relative'>
+									{/* Profile dropdown */}
+									{/* <Menu as='div' className='ml-3 relative'>
                               <div>
                                  <Menu.Button className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
                                     <span className='sr-only'>
@@ -180,25 +180,25 @@ const Header = ({ href }) => {
                                  </Menu.Items>
                               </Transition>
                            </Menu> */}
-                        </div>
-                     </div>
-                  </div>
+								</div>
+							</div>
+						</div>
 
-                  <Disclosure.Panel className='sm:hidden'>
-                     <div className='flex flex-col  backdrop-filter backdrop-blur-lg bg-opacity-30  px-2 pt-2 pb-3 space-y-1'>
-                        {navigation.map((item) => (
-                           <ActiveLink key={item.name} href={item.href} id={ `${item.name}_header_button_m`} data-splitbee-event={`Header clicked ${item.name}_header_button_m`}
-                           >
-                            -   {item.name}
-                           </ActiveLink>
-                        ))}
-                     </div>
-                  </Disclosure.Panel>
-               </>
-            )}
-         </Disclosure>
-      </div>
-  )
+						<Disclosure.Panel className='sm:hidden'>
+							<div className='flex flex-col  backdrop-filter backdrop-blur-lg bg-opacity-30  px-2 pt-2 pb-3 space-y-1'>
+								{navigation.map((item) => (
+									<ActiveLink key={item.name} href={item.href} id={`${item.name}_header_button_m`} data-splitbee-event={`Header clicked ${item.name}_header_button_m`}
+									>
+										-   {item.name}
+									</ActiveLink>
+								))}
+							</div>
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
+		</div>
+	)
 }
 
 export default Header
