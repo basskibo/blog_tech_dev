e484c24f18c62fb44d0b2304307b47759d438c45
Bojan Jagetic <jagetic.bojan@gmail.com>
2024-04-05 18:42:28 +0200

    feat: rss feed added

--- pages/routes/blog.js
+++ pages/routes/blog.js
@@ -6,37 +6,42 @@ import path from 'path'
 import matter from 'gray-matter'
 
 import 'react-loading-skeleton/dist/skeleton.css'
+import generateRSSFeed from 'utils/generateRSSFeed'
 
 const blog = ({ posts }) => {
-  if (typeof window !== 'undefined' && posts.length > 0) {
-    localStorage.setItem('posts', JSON.stringify(posts))
-  }
-  return <BlogScreen posts={posts} />
+	if (typeof window !== 'undefined' && posts.length > 0) {
+		localStorage.setItem('posts', JSON.stringify(posts))
+	}
+	return <BlogScreen posts={posts} />
 }
 
 export default blog
 
 export const getStaticProps = async () => {
-  const files = getPostFiles()
+	const files = getPostFiles()
 
-  const posts = files.map((filename) => {
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts/blog', filename),
-      'utf-8'
-    )
+	const posts = files.map( (filename) => {
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts/blog', filename),
+			'utf-8'
+		)
 
-    let { data } = matter(markdownWithMeta)
-    data = { ...data, slug: filename.split('.')[0] }
-    // const mdxSource = await serialize(content);
-    return {
-      props: {
-        data
-      }
-    }
-  })
-  return {
-    props: {
-      posts
-    }
-  }
+		let { data } = matter(markdownWithMeta)
+		data = {
+			...data, slug: filename.split('.')[0]
+		}
+		// const mdxSource = await serialize(content);
+		return {
+			props: {
+				data
+			}
+		}
+	})
+	const rssFeed = await generateRSSFeed(posts)
+
+	return {
+		props: {
+			posts
+		}
+	}
 }
