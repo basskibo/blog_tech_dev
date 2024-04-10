b5638a1ffb30afcd18b84c773c7c41e6fd678420
Bojan Jagetic <jagetic.bojan@gmail.com>
2024-04-10 16:20:35 +0200

    fix: eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'eslint:recommended',
-	'prettier',
-    'next'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    // 'react',
-	'prettier'
-  ],
-  rules: {
-    'react/prop-types': 0,
-    // indent: ['error', 'tab'],
-	"react/react-in-jsx-scope": "off",
-	"react/display-name": "off",
-	"react/prop-types": 0,
-	// "indent": ["error", "tab"],
-	"linebreak-style": ["error", "unix"], //'unix'
-	// "quotes": ["error", "single"],
-	// "semi": ["error", "never"],
-	"no-debugger": "off"
+	env: {
+	  browser: true,
+	  es2021: true
+	},
+	extends: [
+	  'plugin:react/recommended',
+	  'standard',
+	  'eslint:recommended',
+	  'prettier',
+	  'next'
+	],
+	overrides: [
+	],
+	parserOptions: {
+	  ecmaVersion: 'latest',
+	  sourceType: 'module'
+	},
+	plugins: [
+	  // 'react',
+	  'prettier'
+	],
+	rules: {
+	  'react/prop-types': 0,
+	  // indent: ['error', 'tab'],
+	  "react/react-in-jsx-scope": "off",
+	  "react/display-name": "off",
+	  "react/prop-types": 0,
+	  "indent": ["error", "tab"],
+	  "linebreak-style": ["error", "unix"], //'unix'
+	  "quotes": ["error", "single"],
+	  "semi": ["error", "never"],
+	  "no-debugger": "off"
+	}
   }
-}
+  
